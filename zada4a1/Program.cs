c697134c2dbc3be1e3d774b83b51fb0eab6b23ac
Ruslan <ri4i23aminov@yandex.ru>
2022-04-26 23:47:04 +0300

    zada4a2

--- zada4a1/Program.cs
+++ zada4a1/Program.cs
@@ -1,35 +1,37 @@
-﻿/* Задайте массив заполненный случайными положительными трёхзначными числами.
- Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2 */
-
-int[] numbers = new int[10];
+Console.WriteLine("Введите длину массива ");
+int Length = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[Length];
 FillArray(numbers);
-WriteArray(numbers);
-
-int even = 0;
-
-for(int i = 0; i < numbers.Length; i++)
+PrintArray(numbers);
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
 {
-    if(numbers[i] % 2 == 0)
+    if (numbers[i] % 2 == 0)
     {
-        even = numbers[i];
-        Console.WriteLine(even);
-    } 
+        count = count + 1;
+    }
+    else
+    {
+        count = count + 0;
+    }
 }
+Console.Write("Количество чётных чисел в массиве " + count);
 
 void FillArray(int[] array)
 {
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         array[i] = new Random().Next(100, 1000);
     }
 }
-
-void WriteArray(int[] array)
+void PrintArray(int[] array1)
 {
-     for(int i = 0; i < array.Length; i++)
-     {
-         Console.Write(array[i] + " ");
-     }
-     Console.WriteLine();
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write(array1[i] + " ");
+    }
+    Console.WriteLine();
 }
