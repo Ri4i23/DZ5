aa0c7fa2b0db91e43f0b8f937056f3b6aece52f1
Ruslan <ri4i23aminov@yandex.ru>
2022-04-26 16:17:56 +0300

    Linedgfhsdfgsjgfsdkjfhsgdfjhgsdghjsgdfsdfsdfstarting

---
+++ zada4a1/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами.
+ Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int[] numbers = new int[10];
+FillArray(numbers);
+WriteArray(numbers);
+
+int even = 0;
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] % 2 == 0)
+    {
+        even = numbers[i];
+        Console.WriteLine(even);
+    } 
+}
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void WriteArray(int[] array)
+{
+     for(int i = 0; i < array.Length; i++)
+     {
+         Console.Write(array[i] + " ");
+     }
+     Console.WriteLine();
+}
